f963ab6fcb922972017ea79a103c389289945e03
Rikardo Anju <juu@gmail.com>
2026-04-08 12:04:21 +0700

    fix: dont crash if .env not found

--- internal/middleware/errorHandler.go
+++ internal/middleware/errorHandler.go
@@ -10,9 +10,17 @@ import (
 var errorLogger *log.Logger
 
 func InitLogger() {
+	if err := os.MkdirAll("./log", 0755); err != nil {
+		log.Println("Cannot create log directory, logging to stdout:", err)
+		errorLogger = log.New(os.Stdout, "", log.LstdFlags)
+		return
+	}
+
 	logFile, err := os.OpenFile("./log/error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
+		log.Println("Cannot open log file, logging to stdout:", err)
+		errorLogger = log.New(os.Stdout, "", log.LstdFlags)
+		return
 	}
 
 	errorLogger = log.New(logFile, "", log.LstdFlags)
